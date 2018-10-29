d7c4888fb70b30edf92a151ce044e48a1548bc28
Jaakko Paikkala <Jaakko.Paikkala@myy.haaga-helia.fi>
2018-10-29 08:54:04 +0200

    added demo-questions lol

--- src/main/java/com/ohjelmistoprojekti/Application.java
+++ src/main/java/com/ohjelmistoprojekti/Application.java
@@ -1,12 +1,35 @@
 package com.ohjelmistoprojekti;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.ohjelmistoprojekti.domain.Question;
 
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner bookDemo(QuestionRepository qRepository) {
+        return (args) -> {
+            
+            log.info("Save questions");
+            qRepository.save(new Question("Mitä laitetta käytät pääasiassa koulutehtävien tekemiseen?"));
+            qRepository.save(new Question("Minkä merkkinen laite on?"));
+            qRepository.save(new Question("Pystytkö käyttämään koulun vaatimia ohjelmistoja koneella?"));
+            qRepository.save(new Question("Pystytkö käyttämään Peppiä laitteella ongelmitta?"));
+            qRepository.save(new Question("Onko ohjeistus riittävä, jos käyttää muita kuin suositeltuja laitteita?"));
+                    
+            log.info("Show questions");
+            for (Question question : qRepository.findAll()) {
+                log.info(question.toString());
+            }
+
+        };
+    }
 }
