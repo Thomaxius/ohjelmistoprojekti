8a5e8d7c1fd020066b4e961b0afe3fe7e03c1715
Tommi Santamaa <Tommi.Santamaa@gmail.com>
2018-11-05 09:16:28 +0200

    Attempt to fix hibernate_sequence error

--- src/main/java/com/ohjelmistoprojekti/domain/Question.java
+++ src/main/java/com/ohjelmistoprojekti/domain/Question.java
@@ -10,7 +10,7 @@ import javax.persistence.Id;
 public class Question {
 
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long questionId;
 	private String questionName;
 
