3eef1b9f4a4d224c2980a8afcc0f543a244c6ea1
Tommi Santamaa <Tommi.Santamaa@gmail.com>
2018-12-06 04:40:10 +0200

    Fix stupid mistake

--- src/main/java/com/ohjelmistoprojekti/QuestionApplication.java
+++ src/main/java/com/ohjelmistoprojekti/QuestionApplication.java
@@ -73,7 +73,7 @@ public class QuestionApplication {
             for (Question question : questionRepository.findAll()) {
                 log.info(question.toString());
             }
-            }
+            
         };
     }
 }
